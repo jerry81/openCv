0914c75d0467a9567e7238a6c14ea846906f1057
Raskolnikov <jerry.tann@opteyes.cn>
2021-03-31 11:50:09 +0800

    feat: tutorial, imRead and imShow question - how to close the display

---
+++ src/imageTutorial.py
@@ -0,0 +1,14 @@
+
+from cv2 import cv2
+import sys
+
+# ord() - integer representing unicode char
+
+img = cv2.imread(cv2.samples.findFile("images/faceSample.jpeg"))
+
+if img is None:
+    sys.exit("Could not read the image.")
+cv2.imshow("Display window", img)
+k = cv2.waitKey(0)
+if k == ord("s"):
+    cv2.imwrite("images/faceSample2.jpeg", img)
