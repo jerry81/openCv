c97cefb13d2c54dd2f7437e8a02c2961a848c047
Raskolnikov <jerry.tann@opteyes.cn>
2021-03-31 11:54:17 +0800

    feat: limit the image show to 7 seconds.  use imwrite

--- src/imageTutorial.py
+++ src/imageTutorial.py
@@ -9,6 +9,6 @@ img = cv2.imread(cv2.samples.findFile("images/faceSample.jpeg"))
 if img is None:
     sys.exit("Could not read the image.")
 cv2.imshow("Display window", img)
-k = cv2.waitKey(0)
+k = cv2.waitKey(7000)
 if k == ord("s"):
     cv2.imwrite("images/faceSample2.jpeg", img)
